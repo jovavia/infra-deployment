9e79a83f8474d58ab5b5b2d1b2c1fba84202a47d
saxenapraveen <praveensaxena0409@gmail.com>
2026-08-27 00:38:05 +0530

    security: template PgBouncer user credentials

--- docker/pgbouncer/start-pgbouncer.sh
+++ docker/pgbouncer/start-pgbouncer.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 set -eu
 
+echo "Generating PgBouncer userlist..."
+
+eval "cat <<EOF
+$(cat /etc/pgbouncer/userlist.template)
+EOF" > /tmp/userlist.txt
+
 echo "Generating PgBouncer configuration..."
 
 eval "cat <<EOF
 $(cat /etc/pgbouncer/pgbouncer.ini.template)
 EOF" > /tmp/pgbouncer.ini
 
-echo "===== PgBouncer Config ====="
-cat /tmp/pgbouncer.ini
-echo "============================"
+echo "Starting PgBouncer..."
 
 exec pgbouncer /tmp/pgbouncer.ini
